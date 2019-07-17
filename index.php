a0fff377948131971e050e89b91c279ba2f10a62
tekkenchinmi <41213617+tekkenchinmi@users.noreply.github.com>
2019-07-17 17:03:53 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,67 @@
+<html>
+    <head>
+        <title>Face Detect Sample</title>
+    </head>
+    <body>
+
+<?php
+// Replace <Subscription Key> with a valid subscription key.
+$ocpApimSubscriptionKey = 'fa72d4e89dbb4af8a46f1a5864dbfca6';
+
+// You must use the same location in your REST call as you used to obtain
+// your subscription keys. For example, if you obtained your subscription keys
+// from westus, replace "westcentralus" in the URL below with "westus".
+$uriBase = 'https://westcentralus.api.cognitive.microsoft.com/face/v1.0/';
+
+$imageUrl =
+    
+
+'https://upload.wikimedia.org/wikipedia/commons/3/37/Dagestani_man_and_woman.jp
+
+g';
+
+// This sample uses the PHP5 HTTP_Request2 package
+// (https://pear.php.net/package/HTTP_Request2).
+require_once 'HTTP/Request2.php';
+
+$request = new Http_Request2($uriBase . '/detect');
+$url = $request->getUrl();
+
+$headers = array(
+    // Request headers
+    'Content-Type' => 'application/json',
+    'Ocp-Apim-Subscription-Key' => $ocpApimSubscriptionKey
+);
+$request->setHeader($headers);
+
+$parameters = array(
+    // Request parameters
+    'returnFaceId' => 'true',
+    'returnFaceLandmarks' => 'false',
+    'returnFaceAttributes' => 'age,gender,headPose,smile,facialHair,glasses,' .
+        'emotion,hair,makeup,occlusion,accessories,blur,exposure,noise');
+$url->setQueryVariables($parameters);
+
+$request->setMethod(HTTP_Request2::METHOD_POST);
+
+// Request body parameters
+$body = json_encode(array('url' => $imageUrl));
+
+// Request body
+$request->setBody($body);
+
+try
+{
+    $response = $request->send();
+    echo "<pre>" .
+        json_encode(json_decode($response->getBody()), JSON_PRETTY_PRINT) . 
+
+"</pre>";
+}
+catch (HttpException $ex)
+{
+    echo "<pre>" . $ex . "</pre>";
+}
+?>
+</body>
+</html>
